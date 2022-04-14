0251889d36fdde31561d5bdb53f3d8db1341a90d
pujasodha <pujasodha@gmail.com>
2022-04-14 16:44:20 -0700

    tailwind & devise views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get 'users', to: 'devise/sessions#new'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
   devise_for :users
