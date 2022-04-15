3916e06b39e311798fe8c68e34b115aa97f1806c
pujasodha <pujasodha@gmail.com>
2022-04-15 00:32:58 -0700

    create posts / show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # def all_posts
+  #   @all_posts ||= Post.all.includes(:user)
+  # end
+
+  def post_params
+    params.require(:post).permit(:title, :message)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get 'users', to: 'devise/sessions#new'
     get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
+  resources :posts, only: %i[index new show create]
+
   devise_for :users
-  root 'home#index'
 end
